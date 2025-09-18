e21a4202578f2b1ecdad9c77a76e0e025293a20b
Ted Zhang <83065920+tedzhangzg@users.noreply.github.com>
2025-09-18 13:35:34 +0800

    Update urls.sh

--- urls.sh
+++ urls.sh
@@ -1,1 +1,40 @@
+#!/bin/sh
 
+# urls.sh
+# ==================================================
+# Description
+# ==================================================
+# Usage
+# ==================================================
+
+
+# echo "Starting urls.sh ..."
+
+# Download
+# curl -L -O "https://raw.githubusercontent.com/tedzhangzg/scripts/main/urls.sh"
+# dot sourced into all scripts
+# source ./urls.sh
+
+# To download and include
+# wget -O - https://raw.githubusercontent.com/tedzhangzg/shellscripts/main/urls.sh | bash
+
+
+
+# Java JRE JDK
+url_javajre_8_x64="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_202-b08/1961070e4c9b4e26a04e7f5a083f551e/linux-i586/jre-8u202-linux-x64.tar.gz"
+url_javajre_8_x86="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_202-b08/1961070e4c9b4e26a04e7f5a083f551e/linux-i586/jre-8u202-linux-i586.tar.gz"
+url_javajre_10_x64="https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/linux-i586/jre-10.0.2_linux-x64_bin.tar.gz"
+url_javajdk_8_x64="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_202-b08/1961070e4c9b4e26a04e7f5a083f551e/linux-i586/jdk-8u202-linux-x64.tar.gz"
+url_javajdk_8_x86="https://javadl.oracle.com/webapps/download/GetFile/1.8.0_202-b08/1961070e4c9b4e26a04e7f5a083f551e/linux-i586/jdk-8u202-linux-i586.tar.gz"
+url_javajdk_10_x64="https://javadl.oracle.com/webapps/download/GetFile/10.0.2+13/19aef61b38124481863b1413dce1855f/linux-i586/jdk-10.0.2_linux-x64_bin.tar.gz"
+
+
+
+# echo " "
+
+# echo "Terminating urls.sh ..."
+
+
+# ==================================================
+# Notes
+# ==================================================
